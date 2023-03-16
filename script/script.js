6e2f8465194b9aa2000c9289a138d4ed0de81f9e
jigar <jigar.k@simformsolutions.com>
2023-03-16 17:05:41 +0530

    functionality added

--- script/script.js
+++ script/script.js
@@ -1,16 +1,19 @@
+let flag;
 // Validation for Input
-function validateForm() {
+function validateForm(name, description, price, image) {
   // Get references to the form elements
-  var nameInput = document.getElementById("name");
-  var priceInput = document.getElementById("price");
-  var descriptionInput = document.getElementById("description");
-  var image = document.getElementById("inputGroupFile01");
-
-  // Get the values from the input fields
-  var name = nameInput.value.trim();
-  var price = priceInput.value.trim();
+  // var nameInput = document.getElementById("name");
+  // var priceInput = document.getElementById("price");
+  // var descriptionInput = document.getElementById("description");
+  // var image = document.getElementById("inputGroupFile01");
+  // var name = name
+  // var price = price
+  // var descriptionInput = description
+  // var image = image
 
   // Validate name input
+  // console.log(name)
+
   if (name === "") {
     alert("Please enter your name");
     return false;
@@ -26,33 +29,44 @@ function validateForm() {
     alert("Please enter a valid price number that does not start with zero");
     return false;
   }
-
   // Validate description input
-  if (descriptionInput.value.length > 50) {
+  if (description.length > 50) {
     alert("Description can be maximum 50 characters");
     return false;
   }
 
+  if (flag == true) {
+    if (image.length === 0) {
+      alert("Please attach an image");
+      return true;
+    }
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+    if (!allowedExtensions.exec(image.files[0].name)) {
+      alert("Please attach a valid image file (jpg, jpeg, png, or gif)");
+      image.value = "";
+      return false;
+    }
+
+    // Check the file size of the uploaded image
+    var fileSize = image.files[0].size / 1024; // in KB
+    if (fileSize > 750) {
+      alert("Please attach an image that is smaller than 750KB");
+      image.value = "";
+      return false;
+    }
+  }
+  if (flag == false) {
+    if (image.length === 0) {
+      alert("Please attach an image");
+      return true;
+    }
+
+  }
   // Validate image input
-  if (image.files.length === 0) {
-    alert("Please attach an image");
-    return false;
-  }
-
-  var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-  if (!allowedExtensions.exec(image.files[0].name)) {
-    alert("Please attach a valid image file (jpg, jpeg, png, or gif)");
-    image.value = "";
-    return false;
-  }
-
-  // Check the file size of the uploaded image
-  var fileSize = image.files[0].size / 1024; // in KB
-  if (fileSize > 750) {
-    alert("Please attach an image that is smaller than 750KB");
-    image.value = "";
-    return false;
-  }
+  // if (image.files.length === 0) {
+  //   alert("Please attach an image");
+  //   return false;
+  // }
 
   return true;
 }
@@ -114,11 +128,13 @@ function showData() {
 showData();
 // Function to add Data
 function AddData() {
-  if (validateForm() == true) {
-    let name = document.getElementById("name").value;
-    let price = document.getElementById("price").value;
-    let description = document.getElementById("description").value;
-    let image = document.getElementById("inputGroupFile01");
+  flag = true;
+  let name = document.getElementById("name").value;
+  let price = document.getElementById("price").value;
+  let description = document.getElementById("description").value;
+  let image = document.getElementById("inputGroupFile01");
+
+  if (validateForm(name, description, price, image) == true) {
     const reader = new FileReader();
 
     let productList;
@@ -127,11 +143,11 @@ function AddData() {
     } else {
       productList = JSON.parse(localStorage.getItem("productList"));
     }
-    
+
     // generate new ID by incrementing the highest existing ID
     let id = 1;
     if (productList.length > 0) {
-      let ids = productList.map(product => product.id);
+      let ids = productList.map((product) => product.id);
       id = Math.max(...ids) + 1;
     }
 
@@ -177,6 +193,7 @@ function deleteData(index) {
 // Function to update/Edit the data in local storage
 function editData(index) {
   var productList;
+  flag = false;
   if (localStorage.getItem("productList") == null) {
     productList = [];
   } else {
@@ -206,21 +223,27 @@ function editData(index) {
   };
 
   document.querySelector("#update").onclick = function () {
+    console.log(document.getElementById("name-edit").value);
     productList[index].id = document.getElementById("id-edit").value;
     productList[index].name = document.getElementById("name-edit").value;
     productList[index].price = document.getElementById("price-edit").value;
-    productList[index].description = document.getElementById("description-edit").value;
-
-    localStorage.setItem("productList", JSON.stringify(productList));
-    location.reload();
-    showData();
-    document.getElementById("id-edit").value = "";
-    document.getElementById("name-edit").value = "";
-    document.getElementById("price-edit").value = "";
-    document.getElementById("description-edit").value = "";
-    document.getElementById("close-btn").click();
-    alert("Data Updated Successfully");
+    productList[index].description =
+      document.getElementById("description-edit").value;
+    console.log(Object.values(productList[index]));
+    if (validateForm(...Object.values(productList[index]).slice(1))) {
+      localStorage.setItem("productList", JSON.stringify(productList));
+      location.reload();
+      showData();
+      document.getElementById("id-edit").value = "";
+      document.getElementById("name-edit").value = "";
+      document.getElementById("price-edit").value = "";
+      document.getElementById("description-edit").value = "";
+      document.getElementById("close-btn").click();
+      alert("Data Updated Successfully");
+    }
   };
+
+
 }
 // Filter Functions
 const selectElem = document.querySelector("#sort-select");
@@ -283,7 +306,8 @@ function filteredData(sortedProduct) {
         </div>
       </div>
     </div>`;
-  } else { sortedProduct.forEach(function (element, index) {
+  } else {
+    sortedProduct.forEach(function (element, index) {
       html += `<div class'card-body'>
        <div class='row gx-2'>
        <div class='col'>
@@ -346,8 +370,8 @@ function searchProduct(sortedItem) {
       </div>
     </div>`;
   } else {
-  sortedItem.forEach(function (element, index) {
-    html += `<div class'card-body'>
+    sortedItem.forEach(function (element, index) {
+      html += `<div class'card-body'>
        <div class='row gx-2'>
        <div class='col'>
        <div class='p-3'>
@@ -368,12 +392,13 @@ function searchProduct(sortedItem) {
         <button onclick='deleteData("${index}")' type='button' class='btn btn-outline-danger'><i class='fa-solid fa-trash'></i> Delete</button>
        </div>
        </div>
+      
        </div>
        </div>
        </div>
        </div>`;
-  });
-}
+    });
+  }
   document.querySelector("#curd-table").classList.add("d-none");
   document.querySelector("#sort-table").innerHTML = html;
 }
